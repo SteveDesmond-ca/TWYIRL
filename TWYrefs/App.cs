39317832ac0d55cfe66ced1d281463182540e7db
Steve Desmond <Steve@ecoAPM.com>
2022-03-30 10:12:16 -0400

    Add Markdown filtering logic

--- TWYrefs/App.cs
+++ TWYrefs/App.cs
@@ -7,79 +7,86 @@ namespace TWYrefs;
 
 public class App
 {
-    private readonly IFileSystem _fs;
-    private readonly Action<string> _log;
-    private readonly Func<int, int> _rng;
-    private readonly ITwitterClient _twitter;
+	private readonly IFileSystem _fs;
+	private readonly Action<string> _log;
+	private readonly Func<int, int> _rng;
+	private readonly ITwitterClient _twitter;
 
-    public App(IFileSystem fs, ITwitterClient twitter, Func<int, int> rng, Action<string> log)
-    {
-        _fs = fs;
-        _log = log;
-        _rng = rng;
-        _twitter = twitter;
-    }
+	public App(IFileSystem fs, ITwitterClient twitter, Func<int, int> rng, Action<string> log)
+	{
+		_fs = fs;
+		_log = log;
+		_rng = rng;
+		_twitter = twitter;
+	}
 
-    public async Task<int> Run()
-    {
-        try
-        {
-            var refs = GetRefs();
-            var tweets = GetTweets();
-            var random = GetNewRef(await refs, await tweets);
-            await Tweet(random);
-            return 0;
-        }
-        catch (Exception e)
-        {
-            _log(e.Message);
-            return 1;
-        }
-    }
+	public async Task<int> Run()
+	{
+		try
+		{
+			var lyrics = GetLyrics("refs.txt");
+			var tweets = GetTweets("TWY_refs");
+			var random = GetNewRef(await lyrics, await tweets);
+			await Tweet(random);
+			return 0;
+		}
+		catch (Exception e)
+		{
+			_log(e.Message);
+			return 1;
+		}
+	}
 
-    private string GetNewRef(string[] refs, IEnumerable<ITweet> tweets)
-        => GetNewRef(refs, tweets.Select(t => t.Text).ToArray());
+	private string GetNewRef(IReadOnlyList<string> lines, IReadOnlyList<ITweet> tweets)
+	{
+		string[] lyrics = lines.Where(l => !string.IsNullOrWhiteSpace(l) && !l.StartsWith("#")).ToArray();
+		string[] tweets1 = tweets.Select(t => t.Text).ToArray();
+		return GetNewRef(lyrics, tweets1);
+	}
 
-    private string GetNewRef(IReadOnlyList<string> refs, IReadOnlyList<string> tweets)
-    {
-        while (true)
-        {
-            var random = _rng(refs.Count);
-            var tweet = refs[random];
+	private string GetNewRef(IReadOnlyList<string> lyrics, IReadOnlyList<string> tweets)
+	{
+		var attempts = 0;
+		while (attempts++ < lyrics.Count)
+		{
+			var random = _rng(lyrics.Count);
+			var tweet = lyrics[random];
 
-            if (!tweets.Contains(tweet))
-            {
-                return tweet;
-            }
+			if (!tweets.Contains(tweet))
+			{
+				return tweet;
+			}
 
-            _log($@"Already tweeted ""{tweet}""");
-        }
-    }
+			_log($"Already tweeted [{tweet}]");
+		}
 
-    private async Task Tweet(string quote)
+		throw new KeyNotFoundException("No new lyrics to tweet");
+	}
 
-    {
-        _log($@"Tweeting ""{quote}""");
-        await _twitter.Tweets.PublishTweetAsync(quote);
-    }
+	private async Task Tweet(string quote)
 
-    private async Task<string[]> GetRefs()
-        => await _fs.File.ReadAllLinesAsync("refs.txt");
+	{
+		_log($"Tweeting [{quote}]");
+		await _twitter.Tweets.PublishTweetAsync(quote);
+	}
 
-    private async Task<ITweet[]> GetTweets()
-    {
-        var options = new GetUserTimelineParameters("TWY_refs")
-        {
-            PageSize = 365 * 2
-        };
+	private async Task<string[]> GetLyrics(string file)
+		=> await _fs.File.ReadAllLinesAsync(file);
 
-        var tweets = await _twitter.Timelines.GetUserTimelineAsync(options);
+	private async Task<ITweet[]> GetTweets(string user)
+	{
+		var options = new GetUserTimelineParameters(user)
+		{
+			PageSize = 365 * 2
+		};
 
-        foreach (var tweet in tweets)
-        {
-            Console.WriteLine(tweet.Text);
-        }
+		var tweets = await _twitter.Timelines.GetUserTimelineAsync(options);
 
-        return tweets;
-    }
+		foreach (var tweet in tweets)
+		{
+			Console.WriteLine(tweet.Text);
+		}
+
+		return tweets;
+	}
 }
